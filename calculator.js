72c8eb7f4f5475c6966144715c0db0d95db6eb9f
wartinillo221 <axelcortez84@gmail.com>
2021-09-30 11:00:32 -0500

    ejercicio de testing

--- calculator.js
+++ calculator.js
@@ -1,13 +1,47 @@
-function sum(a, b) {
-  return;
+function sum(a , b ) {
+  let resultado = 0
+  if ( !a && !b){
+    resultado = 0
+  }else {
+      if (!a){
+        resultado = b + 0 
+      } else if ( !b){
+        resultado = a + 0 
+      }else {
+        resultado = a + b
+      }
+  }
+  return resultado ;
 }
 
-function subtract(a, b) {
-  return;
+function subtract(a , b ) {
+  let resultado = 0
+  if ( !a && !b){
+    resultado = 0
+  }else {
+      if (!a){
+        resultado = b - 0 
+      } else if ( !b){
+        resultado = a - 0 
+      }else {
+        resultado = a - b
+      }
+  }
+  return resultado ;
 }
 
 function divide(a, b) {
-  return;
+  let resultado = 0
+  if (a != 0){
+    if(b != 0){
+      resultado = a/b
+    }else{
+      throw new Error('el valor de "b" no puede ser 0');
+    }
+  }else{
+    throw new Error('el valor de "a" no puede ser 0');
+  }
+  return resultado;
 }
 
 function multiply(a, b) {
